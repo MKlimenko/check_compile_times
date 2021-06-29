be184c75a6bc28172fb2e11cadbcff5a9f1b4d3a
m.klimenko <mklimenko29@gmail.com>
2021-06-29 23:59:42 +0300

    Add latest boost as a conan dependency
    	Add boost json and pfr libraries

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
-mkdir build
+conan install boost/1.76.0@ --build=never -if build/boost -s build_type=Release -g CMakeDeps
 cd build
 
-for i in {0..9}
+for i in {0..1}
 do
 	mkdir build_$i
 	cd build_$i
-	cmake -G Ninja ../..
+	cmake -G Ninja ../.. -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$PWD/../boost
 	cmake --build .
 	cd ..
 done
 
-cmake -G Ninja .. -DSKIP_EVALUATION=true
+cmake -G Ninja .. -DSKIP_EVALUATION=true -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$PWD/boost
 cmake --build .
 ./CheckCompileTimes
